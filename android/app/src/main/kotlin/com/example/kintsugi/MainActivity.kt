6e5384bb7744caf3a38ac3d11ecf3d97cc22eff6
BangMach <50797948+BangMach@users.noreply.github.com>
2022-06-05 20:21:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kintsugi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kintsugi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main4.dart
@@ -0,0 +1,38 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'app.dart';
+//
+// ThemeMode appTheme = ThemeMode.system; //dark / light
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // debugShowCheckedModeBanner: false,
+//       title: 'db Meter',
+//       home: NoiseApp(),
+//       darkTheme: ThemeData.dark().copyWith(
+//         textTheme: ThemeData.dark().textTheme.apply(
+//           fontFamily: GoogleFonts.comfortaa().fontFamily,
+//         ),
+//         primaryTextTheme: ThemeData.dark().textTheme.apply(
+//           fontFamily: GoogleFonts.comfortaa().fontFamily,
+//         ),
+//         accentTextTheme: ThemeData.dark().textTheme.apply(
+//           fontFamily: GoogleFonts.comfortaa().fontFamily,
+//         ),
+//       ),
+//       theme: ThemeData(
+//         fontFamily: GoogleFonts.comfortaa().fontFamily,
+//       ),
+//       themeMode: appTheme,
+//     );
+//   }
+// }
