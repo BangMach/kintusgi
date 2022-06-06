ef688017ea1137797cd88dd001b7f92afb58d927
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-06-06 15:08:19 +0700

    Removed redundant code

--- lib/old/main.dart
+++ lib/old/main.dart
@@ -205,115 +205,42 @@ class _SpeechScreenState extends State<SpeechScreen> {
   double _cofidence = 1.0;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-              "Confidence Level is: ${(_cofidence * 100.0).toStringAsFixed(1)}%"),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: AvatarGlow(
-          animate: _isListening,
-          glowColor: Theme.of(context).primaryColor,
-          endRadius: 75.0,
-          duration: const Duration(milliseconds: 2000),
-          repeat: true,
-          child: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-          ),
-        ),
-        body: SingleChildScrollView(
-            reverse: true,
-            child: Container(
-                padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-                child: GestureDetector(
-                  onDoubleTap: () {
-                    print("double press clicked");
-                  },
-                  child: Text(_text
-                      // words: _highlights,
-                      // textStyle: const TextStyle(
-                      //   fontSize: 32.0,
-                      //   color: Colors.black,
-                      //   fontWeight: FontWeight.w400,
-                      ),
-                ))));
-
-    // throw UnimplementedError();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+            "Confidence Level is: ${(_cofidence * 100.0).toStringAsFixed(1)}%"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: GestureDetector(
+            onDoubleTap: () {
+              print("double press clicked");
+            },
+            child: Text(_text
+                // words: _highlights,
+                // textStyle: const TextStyle(
+                //   fontSize: 32.0,
+                //   color: Colors.black,
+                //   fontWeight: FontWeight.w400,
+                ),
+          ),
+        ),
+      ),
     );
   }
 }
