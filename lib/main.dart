d2b6198305c811e33a7de93d8dad14a7113793d8
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-06-06 15:27:09 +0700

    Added Firebase Configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:kintsugi/core/application.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(Application());
 }
