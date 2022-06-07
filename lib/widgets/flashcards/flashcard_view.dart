d8904894ce07720c77348df27556d837f9c7200d
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-06-07 23:47:51 +0700

    Updated Flash Cards UI

--- lib/widgets/flashcards/flashcard_view.dart
+++ lib/widgets/flashcards/flashcard_view.dart
@@ -2,17 +2,68 @@ import 'package:flutter/material.dart';
 
 class FlashcardView extends StatelessWidget {
   final String text;
+  final bool isFront;
+  final int index;
 
-  FlashcardView({Key key, this.text}) : super(key: key);
+  FlashcardView({
+    Key key,
+    this.text,
+    this.isFront = true,
+    this.index,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
-      child: Center(
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32.0),
+        side: BorderSide(
+          color: Colors.grey,
+          width: 2.0,
+        ),
+      ),
+      color: isFront ? Colors.indigoAccent : Colors.green[700],
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Flashcard #${index + 1}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white70,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Opacity(
+                opacity: 0,
+                child: Text(
+                  'Flashcard #${index + 1}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
